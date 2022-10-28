fbe30db0812f58bda518ea38865be2faf3df4e63
Aldrin Baroi <aldrin.baroi@gmail.com>
2022-10-28 18:52:45 +0000

    Fixed to add args to proxy command & fixed help command

--- main.sh
+++ main.sh
@@ -12,23 +12,35 @@ case $category in
 		case $command in
 			create) createCluster $@ ;;
 			delete) deleteCluster $@ ;;
-			help) clusterHelp ;;
+			help|*) clusterHelp ;;
 		esac
 		;;
 	dashboard)
 		case $command in
 			setup) setupDashboard $@ ;;
 			remove) removeDashboard $@ ;;
-			help) dashboardHelp ;;
+			help|*) dashboardHelp ;;
 		esac
 		;;
 	proxy)
 		case $command in
-			start) startProxy ;;
-			stop) stopProxy ;;
-			help) proxyHelp ;;
+			start) startProxy $@ ;;
+			stop) stopProxy $@ ;;
+			help|*) proxyHelp ;;
 		esac
 		;; 
+        help|*)
+                cat <<- USAGE
+                            
+		  Usage:
+
+		    kindctl cluster|dashbaord|proxy
+
+		      cluster:   cluster create|delete|help
+		      dashboard: dashboard setup|remove|help
+		      proxy:     start|stop|help
+                            
+                USAGE
 esac
 
 
